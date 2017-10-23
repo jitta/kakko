d339fdaab57fca10168a7d7103b6322952523d9e
Jirat Kijlerdpornpailoj <saakyz@gmail.com>
2017-10-23 19:35:41 +0700

    Setup Storybook React web

---
+++ storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    // These are the reasonable defaults supported by the Node ecosystem.
+    // We also include JSX as a common component filename extension to support
+    // some tools, although we do not recommend using it, see:
+    // https://github.com/facebookincubator/create-react-app/issues/290
+    // `web` extension prefixes have been added for better support
+    // for React Native Web.
+    extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx']
+  },
+  devtool: '#eval-source-map'
+}
